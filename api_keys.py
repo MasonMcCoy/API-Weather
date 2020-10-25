e5ce71a99d3c3f512a902e6e41e0c126135cadbb
Mason McCoy <mmasonmccoy@gmail.com>
2020-10-24 22:48:31 -0500

    Final checks on code. Included observations.

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "4b9ae179b18c6c494e342b8d5938274b"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBLBvS5rJ-iAvCxB4vwnvZrKAEs_VqzJ9M"
